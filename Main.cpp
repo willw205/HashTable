2a48368ab0b8302da758a94e125f733eb418b916
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-02-17 13:44:41 -0800

    Setting up main file

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+
+ #include <cstring>
+ #include <iostream>
+ #include <fstream>
+
+ using namespace std;
+
+ int main(){
+
+   Node** students = new Node*[101];
+   int length = 101;
+
+   ifstream firstNameFile("FirstNames.txt");
+   ifstream lastNameFile("LastNames.txt");
+   
+
+ }
