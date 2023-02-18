52d141588c359d584df58ee8ab12775b5518ee56
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-02-17 20:29:12 -0800

    Comments

--- Node.h
+++ Node.h
@@ -1,18 +1,24 @@
 
+ //William Wong
+ //2/17/23
+
  #ifndef NODE_H
  #define NODE_H
 
  #include <cstring>
  #include <iostream>
+ #include "Student.h"
  using namespace std;
 
  class Node{
+  //Function prototypes below public
   public:
    Node(Student*);
    ~Node();
    Node* getNext();
    Student* getStudent();
    void setNext(Node*);
+  //Intializing variables below private
   private:
    Student* student;
    Node* next;
